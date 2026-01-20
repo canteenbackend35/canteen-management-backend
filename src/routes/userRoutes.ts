40e320cd90e35b72377b9eff2ecc4699c110fcad
Vishal Bhalla <vishalbhalla722@gmail.com>
2026-01-20 17:27:57 +0530

    Trying to merge into main

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { getUserOrders } from "../controllers/userController.js";
-import { sendOtp } from "../controllers/userController.js";
+import {
+  getUserOrders,
+  loginUser,
+  signUpUser,
+	sendOtp
+} from "../controllers/userController.js";
+import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-//router.get("/:userId/orders", getUserOrders);
+router.post("/login", loginUser);
+router.post("/signup", signUpUser);
+router.get("/orders", auth, getUserOrders);
 router.post("/send-otp", sendOtp);
 
 export default router;
